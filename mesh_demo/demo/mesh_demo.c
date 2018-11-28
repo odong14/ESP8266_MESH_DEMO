739522a75b09d5376fe258799aa720dccd72afb5
odong14 <muhammad.adli.riz@gmail.com>
2018-11-28 14:58:23 +0700

    sudah ada fungsi bcast

--- mesh_demo/demo/mesh_demo.c
+++ mesh_demo/demo/mesh_demo.c
@@ -15,18 +15,19 @@
 #include "espconn.h"
 #include "user_config.h"
 
-#define MESH_DEMO_PRINT  ets_printf
-#define MESH_DEMO_STRLEN ets_strlen
-#define MESH_DEMO_MEMCPY ets_memcpy
-#define MESH_DEMO_MEMSET ets_memset
-#define MESH_DEMO_FREE   os_free
+#define MESH_PRINT  ets_printf
+#define MESH_STRLEN ets_strlen
+#define MESH_MEMCPY ets_memcpy
+#define MESH_MEMSET ets_memset
+#define MESH_FREE   os_free
+#define ROOT        1
 
 static esp_tcp ser_tcp;
 static struct espconn ser_conn;
 
-void esp_mesh_demo_test();
+void esp_mesh_test();
 void mesh_enable_cb(int8_t res);
-void esp_mesh_demo_con_cb(void *);
+void esp_mesh_con_cb(void *);
 void esp_recv_entrance(void *, char *, uint16_t);
 
 void esp_recv_entrance(void *arg, char *pdata, uint16_t len)
@@ -38,7 +39,7 @@ void esp_recv_entrance(void *arg, char *pdata, uint16_t len)
     uint8_t *resp = "{\"rsp_key\":\"rsp_key_value\"}";
     struct mesh_header_format *header = (struct mesh_header_format *)pdata;
 
-    MESH_DEMO_PRINT("recv entrance\n");
+    MESH_PRINT("recv entrance\n");
     
     if (!pdata)
         return;
@@ -48,7 +49,7 @@ void esp_recv_entrance(void *arg, char *pdata, uint16_t len)
     if (!espconn_mesh_get_usr_data(header, &usr_data, &usr_data_len)) 
         return;
 
-    MESH_DEMO_PRINT("user data, len:%d, content:%s\n", &usr_data_len, usr_data);
+    MESH_PRINT("user data, len:%d, content:%s\n", &usr_data_len, usr_data);
     /* 
      * process packet
      * call packet_parser(...)
@@ -66,7 +67,7 @@ void esp_recv_entrance(void *arg, char *pdata, uint16_t len)
      */
 #if 0
     if (!espconn_mesh_get_src_addr(header, &src) || !espconn_mesh_get_dst_addr(header, &dst)) {
-        MESH_DEMO_PRINT("get addr fail\n");
+        MESH_PRINT("get addr fail\n");
         return;
     }
 
@@ -76,7 +77,7 @@ void esp_recv_entrance(void *arg, char *pdata, uint16_t len)
                             false, // not p2p packet
                             true,  // piggyback congest request
                             M_PROTO_JSON,  // packe with JSON format
-                            MESH_DEMO_STRLEN(resp),  // data length
+                            MESH_STRLEN(resp),  // data length
                             false, // no option
                             0,     // option len
                             false, // no frag
@@ -85,49 +86,113 @@ void esp_recv_entrance(void *arg, char *pdata, uint16_t len)
                             0,     // frag index
                             0);    // frag length
     if (!header) {
-        MESH_DEMO_PRINT("create packet fail\n");
+        MESH_PRINT("create packet fail\n");
         return;
     }
 
-    if (!espconn_mesh_set_usr_data(header, resp, MESH_DEMO_STRLEN(resp))) {
-        MESH_DEMO_PRINT("set user data fail\n");
-        MESH_DEMO_FREE(header);
+    if (!espconn_mesh_set_usr_data(header, resp, MESH_STRLEN(resp))) {
+        MESH_PRINT("set user data fail\n");
+        MESH_FREE(header);
         return;
     }
 
     if (espconn_mesh_sent(&ser_conn, (uint8_t *)header, header->len)) {
-        MESH_DEMO_PRINT("mesh sent fail\n");
-        MESH_DEMO_FREE(header);
+        MESH_PRINT("mesh sent fail\n");
+        MESH_FREE(header);
         return;
     }
 
-    MESH_DEMO_FREE(header);
+    MESH_FREE(header);
 #endif
 }
 
-void esp_mesh_demo_con_cb(void *arg)
+void ICACHE_FLASH_ATTR
+mesh_json_bcast()
+{
+    char buf[32];
+    uint8_t src[6];
+    uint8_t dst[6];
+    struct mesh_header_format *header = NULL;
+
+    if (!wifi_get_macaddr(STATION_IF, src)) {
+        MESH_PRINT("bcast get sta mac fail\n");
+        return;
+    }
+
+    os_memset(buf, 0, sizeof(buf));
+
+    os_sprintf(buf, "%s", "{\"bcast\":\"");
+    os_sprintf(buf + os_strlen(buf), MACSTR, MAC2STR(src));
+    os_sprintf(buf + os_strlen(buf), "%s", "\"}\r\n");
+
+    os_memset(dst, 0, sizeof(dst));  // use bcast to get all the devices working in mesh from root.
+    header = (struct mesh_header_format *)espconn_mesh_create_packet(
+                            dst,     // destiny address (bcast)
+                            src,     // source address
+                            false,   // not p2p packet
+                            true,    // piggyback congest request
+                            M_PROTO_JSON,   // packe with JSON format
+                            os_strlen(buf), // data length
+                            false,   // no option
+                            0,       // option total len
+                            false,   // no frag
+                            0,       // frag type, this packet doesn't use frag
+                            false,   // more frag
+                            0,       // frag index
+                            0);      // frag length
+    if (!header) {
+        MESH_PRINT("bcast create packet fail\n");
+        return;
+    }
+
+    if (!espconn_mesh_set_usr_data(header, buf, os_strlen(buf))) {
+        MESH_PRINT("bcast set user data fail\n");
+        MESH_FREE(header);
+        return;
+    }
+
+    if (espconn_mesh_sent(&ser_conn, (uint8_t *)header, header->len)) {
+        MESH_PRINT("bcast mesh is busy\n");
+        MESH_FREE(header);
+        espconn_mesh_connect(&ser_conn);
+        return;
+    }
+
+    MESH_FREE(header);
+}
+
+void ICACHE_FLASH_ATTR
+mesh_json_bcast_init()
+{
+    static os_timer_t bcast_timer;
+    os_timer_disarm(&bcast_timer);
+    os_timer_setfn(&bcast_timer, (os_timer_func_t *)mesh_json_bcast, NULL);
+    os_timer_arm(&bcast_timer, 18000, true);
+}
+
+void esp_mesh_con_cb(void *arg)
 {
     static os_timer_t tst_timer;
     struct espconn *server = (struct espconn *)arg;
 
-    MESH_DEMO_PRINT("esp_mesh_demo_con_cb\n");
+    MESH_PRINT("esp_mesh_con_cb\n");
 
     if (server != &ser_conn) {
-        MESH_DEMO_PRINT("con_cb, para err\n");
+        MESH_PRINT("con_cb, para err\n");
         return;
     }
 
     os_timer_disarm(&tst_timer);
-    os_timer_setfn(&tst_timer, (os_timer_func_t *)esp_mesh_demo_test, NULL);
+    os_timer_setfn(&tst_timer, (os_timer_func_t *)esp_mesh_test, NULL);
     os_timer_arm(&tst_timer, 5000, true);
 }
 
 void mesh_enable_cb(int8_t res)
 {
-	MESH_DEMO_PRINT("mesh_enable_cb\n");
+	MESH_PRINT("mesh_enable_cb\n");
 
     if (res == MESH_OP_FAILURE) {
-	    MESH_DEMO_PRINT("enable mesh fail\n");
+	    MESH_PRINT("enable mesh fail\n");
         return;
     }
 
@@ -138,22 +203,22 @@ void mesh_enable_cb(int8_t res)
      * if you want to sent packet to server/mobile, please build normal packet (uincast packet)
      * if you want to sent bcast/mcast packet, please build bcast/mcast packet
      */
-    MESH_DEMO_MEMSET(&ser_conn, 0 ,sizeof(ser_conn));
-    MESH_DEMO_MEMSET(&ser_tcp, 0, sizeof(ser_tcp));
+    MESH_MEMSET(&ser_conn, 0 ,sizeof(ser_conn));
+    MESH_MEMSET(&ser_tcp, 0, sizeof(ser_tcp));
 
-    MESH_DEMO_MEMCPY(ser_tcp.remote_ip, server_ip, sizeof(server_ip));
+    MESH_MEMCPY(ser_tcp.remote_ip, server_ip, sizeof(server_ip));
     ser_tcp.remote_port = server_port;
     ser_tcp.local_port = espconn_port();
     ser_conn.proto.tcp = &ser_tcp;
 
-    if (espconn_regist_connectcb(&ser_conn, esp_mesh_demo_con_cb)) {
-        MESH_DEMO_PRINT("regist con_cb err\n");
+    if (espconn_regist_connectcb(&ser_conn, esp_mesh_con_cb)) {
+        MESH_PRINT("regist con_cb err\n");
         espconn_mesh_disable(NULL);
         return;
     }
 
     if (espconn_regist_recvcb(&ser_conn, esp_recv_entrance)) {
-        MESH_DEMO_PRINT("regist recv_cb err\n");
+        MESH_PRINT("regist recv_cb err\n");
         espconn_mesh_disable(NULL);
         return;
     }
@@ -165,14 +230,17 @@ void mesh_enable_cb(int8_t res)
      */
 
     if (espconn_mesh_connect(&ser_conn)) {
-        MESH_DEMO_PRINT("connect err\n");
+        MESH_PRINT("connect err\n");
         espconn_mesh_disable(NULL);
         return;
     }
+    
+    mesh_json_bcast_init();
 }
 
-void esp_mesh_demo_test()
+void esp_mesh_test()
 {
+    char buf[32];
     uint8_t src[6];
     uint8_t dst[6];
     struct mesh_header_format *header = NULL;
@@ -181,16 +249,23 @@ void esp_mesh_demo_test()
      * the mesh data can be any content
      * it can be string(json/http), or binary(MQTT).
      */
-    char *tst_data = "{\"req_key\":\"device2\"}\r\n";
-    // uint8_t tst_data[] = {'a', 'b', 'c', 'd'};}
-    // uint8_t tst_data[] = {0x01, 0x02, 0x03, 0x04, 0x00};
 
     if (!wifi_get_macaddr(STATION_IF, src)) {
-        MESH_DEMO_PRINT("get sta mac fail\n");
+        MESH_PRINT("get sta mac fail\n");
         return;
     }
-    MESH_DEMO_MEMCPY(dst, server_ip, sizeof(server_ip));
-    MESH_DEMO_MEMCPY(dst + sizeof(server_ip), &server_port, sizeof(server_port));
+
+    // char *tst_data = "{\"req_key\":\"device2\"}\r\n";
+    // uint8_t tst_data[] = {'a', 'b', 'c', 'd'};}
+    // uint8_t tst_data[] = {0x01, 0x02, 0x03, 0x04, 0x00};
+    os_memset(buf, 0, sizeof(buf));
+
+    os_sprintf(buf, "%s", "{\"from\":\"");
+    os_sprintf(buf + os_strlen(buf), MACSTR, MAC2STR(src));
+    os_sprintf(buf + os_strlen(buf), "%s", "\"}\r\n");
+
+    MESH_MEMCPY(dst, server_ip, sizeof(server_ip));
+    MESH_MEMCPY(dst + sizeof(server_ip), &server_port, sizeof(server_port));
 
     header = (struct mesh_header_format *)espconn_mesh_create_packet(
                             dst,   // destiny address
@@ -198,7 +273,7 @@ void esp_mesh_demo_test()
                             false, // not p2p packet
                             true,  // piggyback congest request
                             M_PROTO_JSON,  // packe with JSON format
-                            MESH_DEMO_STRLEN(tst_data),  // data length
+                            MESH_STRLEN(buf),  // data length
                             false, // no option
                             0,     // option len
                             false, // no frag
@@ -207,19 +282,19 @@ void esp_mesh_demo_test()
                             0,     // frag index
                             0);    // frag length
     if (!header) {
-        MESH_DEMO_PRINT("create packet fail\n");
+        MESH_PRINT("create packet fail\n");
         return;
     }
 
-    if (!espconn_mesh_set_usr_data(header, tst_data, MESH_DEMO_STRLEN(tst_data))) {
-        MESH_DEMO_PRINT("set user data fail\n");
-        MESH_DEMO_FREE(header);
+    if (!espconn_mesh_set_usr_data(header, buf, MESH_STRLEN(buf))) {
+        MESH_PRINT("set user data fail\n");
+        MESH_FREE(header);
         return;
     }
 
     if (espconn_mesh_sent(&ser_conn, (uint8_t *)header, header->len)) {
-        MESH_DEMO_PRINT("mesh sent fail\n");
-        MESH_DEMO_FREE(header);
+        MESH_PRINT("mesh sent fail\n");
+        MESH_FREE(header);
         /*
          * if fail, we re-enable mesh
          */
@@ -227,10 +302,10 @@ void esp_mesh_demo_test()
         return;
     }
 
-    MESH_DEMO_FREE(header);
+    MESH_FREE(header);
 }
 
-bool esp_mesh_demo_init()
+bool esp_mesh_init()
 {
     struct station_config config;
 
@@ -240,8 +315,8 @@ bool esp_mesh_demo_init()
     /*
      * set the AP password of mesh node
      */
-    if (!espconn_mesh_encrypt_init(MESH_AUTH, MESH_PASSWD, MESH_DEMO_STRLEN(MESH_PASSWD))) {
-        MESH_DEMO_PRINT("set pw fail\n");
+    if (!espconn_mesh_encrypt_init(MESH_AUTH, MESH_PASSWD, MESH_STRLEN(MESH_PASSWD))) {
+        MESH_PRINT("set pw fail\n");
         return false;
     }
 
@@ -251,7 +326,7 @@ bool esp_mesh_demo_init()
      * mac_route_table_size = (4^max_hop - 1)/3 * 6
      */
     if (!espconn_mesh_set_max_hops(MESH_MAX_HOP)) {
-        MESH_DEMO_PRINT("fail, max_hop:%d\n", espconn_mesh_get_max_hops());
+        MESH_PRINT("fail, max_hop:%d\n", espconn_mesh_get_max_hops());
         return false;
     }
 
@@ -259,8 +334,8 @@ bool esp_mesh_demo_init()
      * mesh_ssid_prefix
      * mesh_group_id and mesh_ssid_prefix represent mesh network
      */
-    if (!espconn_mesh_set_ssid_prefix(MESH_SSID_PREFIX, MESH_DEMO_STRLEN(MESH_SSID_PREFIX))) {
-        MESH_DEMO_PRINT("set prefix fail\n");
+    if (!espconn_mesh_set_ssid_prefix(MESH_SSID_PREFIX, MESH_STRLEN(MESH_SSID_PREFIX))) {
+        MESH_PRINT("set prefix fail\n");
         return false;
     }
 
@@ -269,30 +344,30 @@ bool esp_mesh_demo_init()
      * mesh_group_id and mesh_ssid_prefix represent mesh network
      */
     if (!espconn_mesh_group_id_init((uint8_t *)MESH_GROUP_ID, sizeof(MESH_GROUP_ID))) {
-        MESH_DEMO_PRINT("set grp id fail\n");
+        MESH_PRINT("set grp id fail\n");
         return false;
     }
 
     if (!espconn_mesh_server_init((struct ip_addr *)server_ip, server_port)) {
-        MESH_DEMO_PRINT("server_init fail\n");
+        MESH_PRINT("server_init fail\n");
         return false;
     }
 
     /*
      * please change MESH_ROUTER_SSID and MESH_ROUTER_PASSWD according to your router
      */
-    MESH_DEMO_MEMSET(&config, 0, sizeof(config));
-    MESH_DEMO_MEMCPY(config.ssid, MESH_ROUTER_SSID, MESH_DEMO_STRLEN(MESH_ROUTER_SSID));
-    MESH_DEMO_MEMCPY(config.password, MESH_ROUTER_PASSWD, MESH_DEMO_STRLEN(MESH_ROUTER_PASSWD));
+    MESH_MEMSET(&config, 0, sizeof(config));
+    MESH_MEMCPY(config.ssid, MESH_ROUTER_SSID, MESH_STRLEN(MESH_ROUTER_SSID));
+    MESH_MEMCPY(config.password, MESH_ROUTER_PASSWD, MESH_STRLEN(MESH_ROUTER_PASSWD));
     /*
      * you can use esp-touch(smart configure) to sent information about router AP to mesh node
      * if you donn't use esp-touch, you should use espconn_mesh_set_router to set router for mesh node
      */
     if (!espconn_mesh_set_router(&config)) {
-        MESH_DEMO_PRINT("set_router fail\n");
+        MESH_PRINT("set_router fail\n");
         return false;
     }
-
+    
     return true;
 }
 
@@ -307,7 +382,7 @@ void user_pre_init(void){}
 *******************************************************************************/
 void user_init(void)
 {
-    if (!esp_mesh_demo_init())
+    if (!esp_mesh_init())
         return;
 
     user_devicefind_init();
